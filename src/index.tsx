81b077eba7d8cc5c2072d69573b91bc93907e61d
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2022-03-04 15:32:19 +0300

    install antd and add form

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'antd/dist/antd.css';
 import './scss/index.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
--- src/pages/signup-page/SignupPage.tsx
+++ src/pages/signup-page/SignupPage.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
-import Form from '../../components/form/Form';
+import { Form, Input, Button, Checkbox } from 'antd';
 
 const SignupPage = () => {
   return (
     <div>
-      <Form />
+      <Form
+        name="signup-form"
+
+        initialValues={{ remember: true }}
+        autoComplete="off"
+        layout='vertical'
+      >
+        <Form.Item
+          label="Имя"
+          name="username"
+          rules={[{ required: true, message: 'Введите имя' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Пароль"
+          name="password"
+          rules={[{ required: true, message: 'Введите пароль' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item name="remember" valuePropName="checked" required>
+          <Checkbox>Согласие на обработку персональных данных</Checkbox>
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   )
 }
