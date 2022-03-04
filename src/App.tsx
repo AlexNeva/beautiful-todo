957322cd083861e29c27295101f3f12229570765
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2022-03-04 17:21:46 +0300

    install and settings firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { getDatabase, ref, set } from "firebase/database";
 import './App.scss';
 import Header from './components/header/Header';
 import SignupPage from './pages/signup-page/SignupPage';
 import TodosPage from './pages/todos-page/TodosPage';
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <div className='container'>
-      <Routes>
-        <Route path='/mytodos' element={<TodosPage />} />
-        <Route path='/signup' element={<SignupPage />} />
-      </Routes>
+const App = () => {
+
+  useEffect(() => {
+    const db = getDatabase();
+    console.log(db);
+
+  }, [])
+
+  return (
+    <div className="App">
+      <Header />
+      <div className='container'>
+        <Routes>
+          <Route path='/mytodos' element={<TodosPage />} />
+          <Route path='/signup' element={<SignupPage />} />
+        </Routes>
+      </div>
+
     </div>
+  )
+};
 
-  </div>
-
-);
 
 export default App;
