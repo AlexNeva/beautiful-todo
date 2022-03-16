25dffb9088cea84f1516edd7d6691a4169f2ed1c
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2022-03-16 16:18:04 +0300

    signout refactoring

--- src/components/auth/signout/Signout.tsx
+++ src/components/auth/signout/Signout.tsx
@@ -1,23 +1,16 @@
-import React, { FC, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { FC } from 'react';
 import { getAuth, signOut } from "firebase/auth";
-import { AuthContext } from '../../context/AuthContext';
-import { routes } from '../../../routes/routes';
 import { message } from 'antd';
 
 const Signout: FC = () => {
 
   type MessagesType = { [property: string]: string };
 
-  const navigate = useNavigate();
-
 
   const messages: MessagesType = {
     error: 'Возникла ошибка. Попробуйте снова',
     success: 'Вы успешно вышли из личного кабинета',
   }
-
-  const { setAuth } = useContext(AuthContext);
 
   const signout = () => {
     const auth = getAuth();
